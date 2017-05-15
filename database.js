407060cce2cf1f30822fb625669d97984658c284
beeva-sergiosantamaria <sergio.santamaria@beeva.com>
2017-05-15 21:04:17 +0200

    new origin methods

--- database.js
+++ database.js
@@ -19,8 +19,8 @@ mongoose.connect('mongodb://localhost', function(err, res) {
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "'GET,PUT,POST,DELETE");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods", "GET, POST,DELETE");
   next();
 });
 
